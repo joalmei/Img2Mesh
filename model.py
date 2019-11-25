8336ea02f1108c74715e7cc62e1b38e8eed657ba
João Victor Almeida <jv.aaguiar@gmail.com>
2019-11-24 21:08:19 -0300

    minor

--- model.py
+++ model.py
@@ -23,10 +23,10 @@ def createNetwork(hidden_size, out_vertices):
 
     model.add(layers.Flatten())
     model.add(layers.Dense(hidden_size, activation='relu',
-                            bias_initializer=tf.random_uniform_initializer(-1,1),
+                            bias_initializer=tf.random_uniform_initializer(-0.01,0.01),
                             kernel_initializer=tf.random_uniform_initializer(-1,1)))
     model.add(layers.Dense(3 * out_vertices, activation='relu',
-                            bias_initializer=tf.random_uniform_initializer(-1,1),
+                            bias_initializer=tf.random_uniform_initializer(-0.01,0.01),
                             kernel_initializer=tf.random_uniform_initializer(-1,1)))
     model.add(layers.Reshape((out_vertices, 3)))
 
