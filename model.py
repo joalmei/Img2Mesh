a40d559fc353f7033d3bb43d096f3f0781e7849c
João Victor Almeida <jv.aaguiar@gmail.com>
2019-11-28 14:01:02 -0300

    minor

--- model.py
+++ model.py
@@ -52,23 +52,54 @@ def createLeanNetwork(hidden_size, out_vertices):
     # model.add(layers.Dense(3 * out_vertices, activation='tanh'))
     # model.add(layers.Reshape((out_vertices, 3)))
 
+    # model = models.Sequential()
+    # model.add(layers.MaxPooling2D((8, 8), input_shape=(1, 400, 400),
+    #                                 data_format='channels_first'))
+
+    # model.add(layers.Conv2D(16, (3,3),
+    #                         data_format='channels_first'))
+
+    # model.add(layers.MaxPooling2D((2, 2),
+    #                                 data_format='channels_first'))
+
+    # model.add(layers.Conv2D(32, (3,3),
+    #                         data_format='channels_first'))
+
+    # model.add(layers.MaxPooling2D((2, 2),
+    #                                 data_format='channels_first'))
+
+    # model.add(layers.Conv2D(64, (3,3),
+    #                         data_format='channels_first'))
+
+    # model.add(layers.MaxPooling2D((2, 2),
+    #                                 data_format='channels_first'))
+
+    # model.add(layers.Flatten())
+    # model.add(layers.Dropout(0.1))
+    # model.add(layers.Dense(hidden_size, activation='relu'))
+
+    # model.add(layers.Dropout(0.1))
+    # model.add(layers.Dense(3 * out_vertices, activation='tanh'))
+    # model.add(layers.Reshape((out_vertices, 3)))
+
+
     model = models.Sequential()
     model.add(layers.MaxPooling2D((8, 8), input_shape=(1, 400, 400),
                                     data_format='channels_first'))
 
+    model.add(layers.Conv2D(4, (3,3),
+                            data_format='channels_first'))
+
+    model.add(layers.MaxPooling2D((4, 4),
+                                    data_format='channels_first'))
+
+    model.add(layers.Conv2D(8, (3,3),
+                            data_format='channels_first'))
+
+    model.add(layers.MaxPooling2D((2, 2),
+                                    data_format='channels_first'))
+
     model.add(layers.Conv2D(16, (3,3),
-                            data_format='channels_first'))
-
-    model.add(layers.MaxPooling2D((2, 2),
-                                    data_format='channels_first'))
-
-    model.add(layers.Conv2D(32, (3,3),
-                            data_format='channels_first'))
-
-    model.add(layers.MaxPooling2D((2, 2),
-                                    data_format='channels_first'))
-
-    model.add(layers.Conv2D(64, (3,3),
                             data_format='channels_first'))
 
     model.add(layers.MaxPooling2D((2, 2),
